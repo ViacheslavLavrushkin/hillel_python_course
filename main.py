ab990754371189c40c5c92152ff5587cdf47f4c7
Viach92 <lavrushkinvv@gmail.com>
2021-05-19 22:41:48 +0300

    HomeWork2

--- main.py
+++ main.py
@@ -47,8 +47,80 @@ def gen_pass():  # length = 20
 
     return generate_password(length)
 
+# crud operation for users
+@app.route('/users/creat/')
+def users_create():
+
+    query_params = request.args
+    first_name = query_params.get('first_name')
+    age = int(query_params.get('age'))
+
+    import sqlite3
+
+    con = sqlite3.connect("./users.db")
+    cur = con.cursor()
+    sql = """
+    INSERTS INTO users
+    values (null, '{first_name}', '{age}')
+    """
+    cur.execute(sql)
+    con.commit()
+    con.close()
+    return 'User was Created'
+
+# crud operation for users delete
+@app.route('/users/delete/')
+def users_delete():
+
+    import sqlite3
+
+    con = sqlite3.connect("./users.db")
+    cur = con.cursor()
+    sql = """
+    DELETE FROM users;
+    """
+    cur.execute(sql)
+    con.commit()
+    con.close()
+    return 'All users were delete'
 
 
+
+@app.route('/users/list/')
+def users_list():
+
+    import sqlite3
+
+    con = sqlite3.connect("./users.db")
+    cur = con.cursor()
+    sql = """
+    SELECT * FROM users;
+    """
+    cur.execute(sql)
+    users_list = cur.fetchall()
+    # breackpoint()
+    con.commit()
+    con.close()
+    return 'All users were delete'
+
+@app.route('/users/update/')
+def users_update():
+
+    query_params = request.args
+    first_name = query_params.get('first_name')
+    age = int(query_params.get('age'))
+
+    import sqlite3
+
+    con = sqlite3.connect("./users.db")
+    cur = con.cursor()
+    sql = """
+    UPDATE users SET age = '{age}';
+    """
+    cur.execute(sql)
+    con.commit()
+    con.close()
+    return 'All users were update'
 
 if __name__ == '__main__':
     app.run(port='5000')
